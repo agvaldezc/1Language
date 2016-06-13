7d51d613dae2f34361ad692bd406c6c124d25790
Alan Valdez <agvaldezc@Alans-MacBook-Pro.local>
2016-06-13 14:06:30 -0500

    Started deashbaords development.

--- 1Language/CoordinatorDashboardController.swift
+++ 1Language/CoordinatorDashboardController.swift
@@ -25,17 +25,17 @@ class CoordinatorDashboardController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    // MARK: - Table view data source
-
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
+//    // MARK: - Table view data source
+//
+//    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+//        // #warning Incomplete implementation, return the number of sections
+//        return 0
+//    }
+//
+//    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        // #warning Incomplete implementation, return the number of rows
+//        return 0
+//    }
     
     //Action triggered by logout button to confirm logout
     @IBAction func logoutConfirmation() {
